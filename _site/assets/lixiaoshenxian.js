bc8ab27d4477d499ed2f256063164ab7508ffd45
liyu <lixiaoshenxian@gmail.com>
2015-03-03 18:18:34 +0800

    css

--- _site/assets/lixiaoshenxian.js
+++ _site/assets/lixiaoshenxian.js
@@ -4,4 +4,10 @@ var _hmt = _hmt || [];
   hm.src = "//hm.baidu.com/hm.js?18e15b9003c998efbdcb42af9f8086c2";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
+
+    $("pre").addClass("prettyprint linenums");
+    var pre = document.createElement("script");
+    pre.src = "/assets/prettify/run_prettify.js?lang-js&skin=tomorrow-night";
+    var pre_s = document.getElementsByTagName("script")[0]; 
+    pre_s.parentNode.insertBefore(pre,pre_s);
 })();
